91024b8723c27659b98806f249d065edf9f71cfc
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-06-20 21:45:10 -0300

    importa firebase

--- letmeask/src/services/firebase.ts
+++ letmeask/src/services/firebase.ts
@@ -1,13 +1,19 @@
 import firebase from 'firebase/app';
 
+import 'firebase/auth';
+import 'firebase/database';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBTyIx5SdSshXlcnVF6AFPhUmQmyirz3dc",
-  authDomain: "letmeask-7a3d7.firebaseapp.com",
-  databaseURL: "https://letmeask-7a3d7-default-rtdb.firebaseio.com",
-  projectId: "letmeask-7a3d7",
-  storageBucket: "letmeask-7a3d7.appspot.com",
-  messagingSenderId: "306054097641",
-  appId: "1:306054097641:web:0cc52b25b7444619401e78"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET ,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID 
 };
 
 firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const database = firebase.database();
