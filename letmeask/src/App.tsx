f84ae0107defd38e382f31db9b400bfae64188d9
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-06-22 21:36:00 -0300

    cria rota para sala

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -2,12 +2,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Home } />
-      <Route path="/rooms/new" component={ NewRoom }/>
+      <Route exact path="/rooms/new" component={ NewRoom }/>
+      <Route path="/rooms/:id" component={ Room }/>
       <NewRoom />
     </Switch>
   );
